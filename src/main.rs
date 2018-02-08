2317e715478509dd59aa7f23a0cd1d1ad30292d5
Nerijus Arlauskas <nercury@gmail.com>
2018-02-08 23:59:41 +0200

    Lesson 1.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+extern crate sdl2;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        // render window contents here
+    }
+}
