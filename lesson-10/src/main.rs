5deccc337b60732f35f558b33e72966c47f9cb72
Nerijus Arlauskas <nercury@gmail.com>
2018-07-11 02:18:55 +0300

    Lesson 10 done

--- lesson-10/src/main.rs
+++ lesson-10/src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 extern crate gl;
 #[macro_use] extern crate failure;
+#[macro_use] extern crate lesson_10_render_gl_derive as render_gl_derive;
 
 pub mod render_gl;
 pub mod resources;
@@ -9,30 +10,13 @@ use render_gl::data;
 use failure::err_msg;
 use resources::Resources;
 
+#[derive(VertexAttribPointers)]
 #[repr(C, packed)]
 struct Vertex {
+    #[location = "0"]
     pos: data::f32_f32_f32,
+    #[location = "1"]
     clr: data::f32_f32_f32,
-}
-
-impl Vertex {
-    fn vertex_attrib_pointers(gl: &gl::Gl) {
-        let stride = std::mem::size_of::<Self>(); // byte offset between consecutive attributes
-
-        let location = 0; // layout (location = 0)
-        let offset = 0; // offset of the first component
-
-        unsafe {
-            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset);
-        }
-
-        let location = 1; // layout (location = 1)
-        let offset = offset + std::mem::size_of::<data::f32_f32_f32>(); // offset of the first component
-
-        unsafe {
-            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset);
-        }
-    }
 }
 
 fn main() {
