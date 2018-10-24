e4e4807ce05a0ed03bc03083c7c89de851bb7735
Nerijus Arlauskas <nercury@gmail.com>
2018-10-24 20:36:26 +0300

    Update failure

--- lesson-24-x/render/src/lib.rs
+++ lesson-24-x/render/src/lib.rs
@@ -6,4 +6,6 @@ pub struct WindowDimensions {
 pub struct WindowSize {
     pub width: i32,
     pub height: i32,
+    pub highdpi_width: i32,
+    pub highdpi_height: i32,
 }
--- lesson-24-x/src/main.rs
+++ lesson-24-x/src/main.rs
@@ -1,13 +1,17 @@
 extern crate gl;
+extern crate sdl2;
 extern crate ui;
 extern crate failure;
+extern crate lesson_24_x_render as render;
 extern crate resources;
-extern crate lesson_24_x_render_gl;
+extern crate lesson_24_x_render_gl as render_gl;
 #[macro_use]
-extern crate lesson_24_x_render_gl_derive;
+extern crate lesson_24_x_render_gl_derive as render_gl_derive;
 
 pub mod profiling;
 pub mod debug;
+
+use failure::err_msg;
 
 fn main() {
     if let Err(e) = run() {
@@ -36,6 +40,29 @@ fn run() -> Result<(), failure::Error> {
         );
 
     let mut mutator = ui::mutator::Mutator::from_schema(&schema);
+
+    let sdl = sdl2::init().map_err(err_msg)?;
+    let video_subsystem = sdl.video().map_err(err_msg)?;
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+    gl_attr.set_accelerated_visual(true);
+    gl_attr.set_double_buffer(true);
+
+    let mut window_size = render::WindowSize {
+        width: 960,
+        height: 600,
+        highdpi_width: 960,
+        highdpi_height: 600
+    };
+
+    let window = video_subsystem
+        .window("Game", window_size.width as u32, window_size.height as u32)
+        .opengl()
+        .resizable()
+        .allow_highdpi()
+        .build()?;
 
     println!("mutator: {:#?}", mutator);
 
